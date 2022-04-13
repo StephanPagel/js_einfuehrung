43d626adfa144ce840159004cf566c19b6b6a62d
StephanPagel <stephan.pagel84@gmail.com>
2022-04-13 17:14:59 +0200

    some addings

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,162 @@ console.log('increment: ' + zahl);
 zahl--;
 console.log('decrement: ' + zahl);
 
+//
+
+// Addition
+let addition_operator = 1 + 1;
+console.log("addition: " + addition_operator);
+
+// Subtraktion
+let subtraktion_operator = 2 - 1;
+console.log("subtraktion: " + subtraktion_operator);
+
+// Multiplikation
+let multiplication_operator = 2 * 2;
+console.log("multiplication: " + multiplication_operator);
+
+// Division
+let division_operator = 4 / 2;
+console.log("division: " + division_operator);
+
+// Modul: zeigt an, was der Rest nach einer Division wäre
+let modulus_operator = 14 % 5;
+console.log("modulus: " + modulus_operator);
+
+//
+
+document.write('<h1>Hello World!</h1>');
+document.write('<p>Have a nice day.</p>')
+
+let myText = "Hello again.";
+document.write(myText);
+
+
+// let name = "Donald";
+let nachname = "Duck";
+document.write('Name: ' + name);
+document.write('Nachname: ' + nachname);
+
+//
+
+a = 12;
+b = a * 3;
+console.log(b);
+
+document.write(b);
+
+console.log(document.URL);
+
+console.log("Hello World!");
+
+console.log("Anton");
+
+let name = "Anton";
+console.log(name);
+
+let age = 28;
+console.log(age);
+
+let job = "Trainer";
+console.log(job);
+
+let married = true;
+console.log(married);
+
+console.log(name + ' ist ' + age + ' Jahre alt, von Beruf ' + job + ' und ist verheiratet: ' + married);
+
+//
+
+let carName = "BMW";
+console.log(carName);
+
+let x = 150;
+console.log(x);
+
+let y = 50;
+console.log(y);
+
+let z = x + y;
+console.log(z);
+
+let firstName = "John"; let lastName = "Doe"; let age = 35;
+console.log(firstName, lastName, age);
+
+//
+
+// var z = 34;
+// var z = 67;
+// console.log(z);
+
+// let x = 34;
+// let x = 67;
+// console.log(x);
+
+// const y = 34;
+// const y = 67;
+// console.log(y);
+
+//
+
+function myFunction() {
+    document.getElementById("test").style.color = "red";
+}
+
+//
+
+window.alert("Hallo Welt");
+window.prompt("Bitte geben Sie Ihren Namen ein.");
+window.prompt("Bitte geben Sie Ihren Namen ein.", "Cancel");
+window.confirm("Stimmen Sie meiner Meinung zu?");
+let alter = prompt("Gib mir bitte dein Alter:");
+let b = 5;
+let a = b * 5 - 3;
+alert(a);
+let mann = "Robert Wadlow ist der größte Mann der Welt : ";
+let grosse = "2,72m";
+alert(mann + grosse);
+
+//
+
+let x = "Hello "
+let y = "World<br>"
+let z = x + y;
+
+document.write(z);
+
+let n = x + " " + y;
+document.write(n);
+
+x = x + "World";
+document.write(x);
+
+let meinString = "Ich bin Erster.";
+let a = "Ich komme auf Platz zwei.";
+meinString += a;
+console.log(meinString);
+
+//
+
+// This is a single line JavaScript comment
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+
+document.write("I have multi-line comments!");
+/*document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");*/
+
+//
+
+let helloWorld = "Hello World!";
+const PI = 3.141592653589793;
+const burjDubai = "828m";
+let fullName = "Jan Schmidt";
+const eifelTower = "324m";
+let camelCase = camelCase;
+let kursStatus = true;
