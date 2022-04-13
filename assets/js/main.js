403d7f6b389c219d3903ba274da178a2486d19e0
StephanPagel <stephan.pagel84@gmail.com>
2022-04-13 17:12:11 +0200

    uebungen beendet

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+let score = 0;
+score += 10;
+console.log('Ergebnis: ' + score);
+
+let zahl = 1;
+zahl = zahl + 1;
+zahl += 1;
+
+zahl++;
+console.log('increment: ' + zahl);
+
+zahl--;
+console.log('decrement: ' + zahl);
+
